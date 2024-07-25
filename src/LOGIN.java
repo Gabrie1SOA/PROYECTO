b26d0d9e9c63960707f604ec4720dfab1df30a26
Gabriel Santiago <gabosanti2003@gmail.com>
2024-07-24 20:41:03 -0500

    update

---
+++ src/LOGIN.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+
+public class LOGIN extends JFrame{
+    private JLabel label;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JLabel label2;
+    private JLabel label3;
+    private JButton LOGINButton;
+    private JPanel panel1;
+
+    public LOGIN() {
+
+        setSize(300,300);
+        setContentPane(panel1);
+
+
+    }
+
+
+}
