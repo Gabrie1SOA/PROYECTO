69ce2fdd61e3f354097fe63b8ab246e712dd37fe
GabrielSOA <150483291+GabrielSantiagoOAguilar@users.noreply.github.com>
2024-08-07 23:09:46 -0500

    Add files via upload

--- libroconnectDB.sql
+++ libroconnectDB.sql
@@ -7,4 +7,23 @@ CREATE TABLE libros (
     Autor VARCHAR(150)
 );
 
+INSERT INTO libros (Id_Libro, Nombre_Libro, Autor) VALUES
+(1, 'Cien años de soledad', 'Gabriel García Márquez'),
+(2, 'Don Quijote de la Mancha', 'Miguel de Cervantes'),
+(3, '1984', 'George Orwell');
+
+
 SELECT * FROM libros;
+
+CREATE TABLE Usuarios (
+	Usuario VARCHAR(50) UNIQUE NOT NULL,
+    contraseña VARCHAR(10) NOT NULL,
+    tipo VARCHAR(30)
+);
+
+SELECT * FROM Usuarios;
+
+INSERT INTO Usuarios (Usuario,contraseña,tipo) VALUES
+("Admin","passadmin","administrador"),
+("User","passuser","usuario")
+;
